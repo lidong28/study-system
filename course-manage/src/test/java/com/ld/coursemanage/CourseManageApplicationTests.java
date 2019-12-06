d2c860041b2dc3c6135a54daba7b428398f297e1
lidong <517440009@qq.com>
2019-12-06 15:10:44 +0800

    first

--- course-manage/src/test/java/com/ld/coursemanage/CourseManageApplicationTests.java
+++ course-manage/src/test/java/com/ld/coursemanage/CourseManageApplicationTests.java
@@ -1,10 +1,10 @@
-package com.ld.repo;
+package com.ld.coursemanage;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class RepoBaseApplicationTests {
+class CourseManageApplicationTests {
 
     @Test
     void contextLoads() {
